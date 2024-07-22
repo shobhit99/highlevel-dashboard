534ae5c341e51e486152a9c87264e3b7da6e0ec8
shobhit99 <shobhitbhosure7@gmail.com>
2024-07-23 05:15:57 +0530

    added title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Highlevel Bulk Actions</title>
+      </head>
       <body className={`${inter.className} text-gray-800`}>
         <div className="flex flex-col min-h-screen max-w-[1280px] mx-auto">
           <header className="border-b border-gray-200 py-4">
